08e85e5dc8422be694f45862a4173fa8426ff36f
agungpg <perdanaagung71@gmail.com>
2024-11-28 12:32:18 +0800

    feat: add 2 special particle, first is the particle that effect the same color, second is the particle that effect all particle

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const hexColors = [
     "#F39C12",  // Orange
     "#1ABC9C"   // Turquoise
 ];
+const goldColor = 'rgb(255, 215, 0)';
 var lastDeduction = 0;
 const speed = 7000;
 const interval = 500;
 var gameIntervalPointer = undefined;
 var CONTAINER = undefined;
+var LIFECONTAINER = undefined;
 var STARTBTN = undefined;
 var STOPBTN = undefined;
 var CONTINUEBTN = undefined;
@@ -22,9 +24,13 @@ var PAUSEBTN = undefined;
 var SCORE = undefined;
 var gameState = 'notPlaying'
 const audio = new Audio('collision.mp3');
+const mediumExplosionAudio = new Audio('medium-explosion.mp3');
+const explosionAudio = new Audio('explosion.mp3');
 var PARTICLESANIMATIONS = {};
 let counter = 0;
-var specialBall = false;
+var specialParticle = false;
+var verySpacialParticle = false;
+var PARTICLESEL = []
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -34,11 +40,20 @@ document.addEventListener("DOMContentLoaded", () => {
     STOPBTN = document.getElementById("stop-btn")
     PAUSEBTN = document.getElementById("pause-btn")
     CONTINUEBTN = document.getElementById("continue-btn")
+    LIFECONTAINER = document.getElementById("life-container")
+    generateLifeHeart()
     STARTBTN.addEventListener('click', startGame)
     PAUSEBTN.addEventListener('click', pauseGame)
     STOPBTN.addEventListener('click', stopGame)
     CONTINUEBTN.addEventListener('click', continueGame)
 })
+function generateLifeHeart() {
+    for (let index = 0; index < 10; index++) {
+        const haert = createElement('img', `heart-${index+1}`, 'heart')
+        haert.src = 'heart.png'
+        LIFECONTAINER.append(haert)
+    }
+}
 
 function generateUniqueKey() {
     return `key_${++counter}`;
@@ -101,9 +116,25 @@ function showPauseBtnAndScore() {
 
 function startGame() {
     gameState = 'playing'
+    document.getElementById("life-container").style.display = 'flex';
     startBtnDisappear(() => {
         showPauseBtnAndScore()
     })
+}
+
+function takeTheHeartLife() {
+    const heart = document.querySelectorAll(".heart");
+    if(heart.length > 0) {
+        heart[heart.length-1].remove()
+    } else {
+        clearInterval(gameIntervalPointer)
+        gameIntervalPointer = undefined;
+        Object.keys(PARTICLESANIMATIONS).forEach(key => PARTICLESANIMATIONS[key]?.pause())
+        hideElementWithAnimation(PAUSEBTN,() => {
+            PAUSEBTN.style.display = 'none'
+            stopGame()
+        })
+    }
 }
 
 function throwParticles() {
@@ -122,9 +153,10 @@ function throwParticles() {
             }
         )
         const key = generateUniqueKey()
+        particle.setAttribute('data-key', key)
         PARTICLESANIMATIONS[key] = particleAnimation
         particleAnimation.onfinish = () => {
-            particle.remove();
+            takeTheHeartLife()
             PARTICLESANIMATIONS[key].cancel();
             delete PARTICLESANIMATIONS[key]
         }
@@ -136,6 +168,10 @@ function throwParticles() {
                 particles.forEach((e) => breakParticle(e))
                 breakParticle(e)
             } else {
+                console.log("e.target.style.backgroundColor: ", e.target.style.backgroundColor)
+                if(e.target.classList.contains("very-special-particle")) {
+                    document.querySelectorAll('.particle').forEach(ne =>  breakParticle(ne))
+                } 
                 breakParticle(e)
             }
         })
@@ -146,11 +182,14 @@ function createParticle() {
     const particle = createElement('div', 'particle', 'particle')
     const randomNumber = Math.floor(Math.random() * 10);
     const color = hexColors[randomNumber]
-    if(specialBall) {
-        particle.style.border = `${color} 1px solid`;
+    if(specialParticle) {
+        particle.style.border = `${color} 3px solid`;
         particle.style.backgroundColor = `#fff`;
-        specialBall = false
+        specialParticle = false
         particle.setAttribute('data-color', color)
+    } else if(verySpacialParticle) {
+        particle.classList.add("very-special-particle")
+        verySpacialParticle = false;
     } else {
         particle.style.backgroundColor = color;
         particle.setAttribute('data-bgcolor', color)
@@ -162,6 +201,19 @@ function createParticle() {
     return particle;
 }
 
+
+function stopAudio() {
+    audio.pause();
+    audio.currentTime = 0; 
+}
+function stopMediumExplosionAudio() {
+    mediumExplosionAudio.pause();
+    mediumExplosionAudio.currentTime = 0; 
+}
+function stopExplosionAudio() {
+    explosionAudio.pause();
+    explosionAudio.currentTime = 0; 
+}
 function breakParticle(e) {
     if(gameState == 'pause') return;
 
@@ -174,20 +226,34 @@ function breakParticle(e) {
     particle.style.top = `${particle.getBoundingClientRect().y}px`
     particle.style.left = `${particle.getBoundingClientRect().x}px`
     particle.style.transition = 'none';
+    const isExplosionPlaying = !explosionAudio.paused && explosionAudio.currentTime > 0 && !explosionAudio.ended;
+    const isMediumExplosionPlaying = !mediumExplosionAudio.paused && mediumExplosionAudio.currentTime > 0 && !mediumExplosionAudio.ended;
+
+    stopMediumExplosionAudio()
+    stopExplosionAudio()
     if(gameState == 'playing'){
         SCORE.textContent++
-        SCORE.style.color =color
+        SCORE.style.color = particle.classList.contains("very-special-particle") ? '#FFD700' :color
+        
     }
-    
-    if(!audio.paused) {
-        audio.pause();
-        audio.currentTime = 0; 
+    if(particle.classList.contains("very-special-particle")) {
+        stopAudio()
+        stopMediumExplosionAudio()
+        explosionAudio.play()
+    } else if(particle.dataset.color && !isExplosionPlaying) {
+        stopAudio()
+        mediumExplosionAudio.play()
+    } else if(!isExplosionPlaying && !isMediumExplosionPlaying){
+        stopAudio()
+        audio.play();
     }
-    audio.play();
 
     for (let index = 0; index < 6; index++) {
         const crash = createElement('div', 'particle-crash', 'particle-crash')
-        crash.style.backgroundColor = color
+        if(particle.dataset.color) {
+            crash.style.border = `1px ${color} solid`;
+            crash.style.backgroundColor = 'transparent'
+        } else crash.style.backgroundColor = color
         particle.append(crash)   
     }
 
@@ -202,24 +268,29 @@ function breakParticle(e) {
                     { transform: `translateX(${x}px) translateY(${y}px)`, opacity: 0 }  // Keyframe 2
                 ],
                 {
-                    duration: 1000, // Animation duration in ms
+                    duration: 500, // Animation duration in ms
                     easing: "linear",
                     iterations: 1,  // Number of times to repeat
                 }
             );
             animation.onfinish = () => {
                 element.style.transform = `translateX(-400px) translateY(${y}px)`
-                element.parentNode.remove();
+                element.parentNode?.remove();
                 element.remove()
             }
         }, 1*index)
     }
+    setTimeout(()=>{
+        PARTICLESANIMATIONS[particle.dataset.key]?.cancel()
+    }, 500)
     const deduction = Math.floor(parseInt(SCORE.textContent)/5)*100;
     if(lastDeduction != deduction) {
         lastDeduction = deduction;
         clearInterval(gameIntervalPointer)
         gameIntervalPointer = undefined;
-        specialBall = true
+      
+        if(SCORE.textContent % 30 == 0)  verySpacialParticle = true
+        else specialParticle = true
         throwParticles()
     }
 }
@@ -295,7 +366,14 @@ function continueGame() {
         CONTINUEBTN.style.display = 'none'
         PAUSEBTN.style.display = 'block'
         showElementWithAnimation(PAUSEBTN, () => {
-            Object.keys(PARTICLESANIMATIONS).forEach(key => PARTICLESANIMATIONS[key].play())
+       
+            Object.keys(PARTICLESANIMATIONS).forEach(key => {     
+                try {
+                    PARTICLESANIMATIONS[key].play()
+                } catch (error) {
+                    
+                }
+            })
             setTimeout(throwParticles, 100)
         })
     })
